59cd7e9efec1efff8c292cc2e58e99ff910cb208
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-04-11 01:12:35 -0300

    se agregar page de administrador he inicio de administrador

--- assets/js/ingreso.js
+++ assets/js/ingreso.js
@@ -2,11 +2,13 @@ const usuario = document.querySelector('[data-usuario]');
 const contraseña = document.querySelector('[data-contraseña]');
 const btn = document.querySelector('[data-btn]');
 function ingreso(){
-    if(usuario.value != 'usuario' && contraseña.value != 'usuario'){
-        alert('Usuario o contraseña incorracto.')
-    }else{
-
+    if(usuario.value == 'usuario' && contraseña.value == 'usuario'){
         window.location.href = './assets/pages/principal.html';
+    }else if(usuario.value == 'admin' && contraseña.value == 'admin'){
+        window.location.href = './assets/pages/administrador.html';
+    }else{
+        alert('Usuario o contraseña incorrecto.')
+
     }
 }
 
