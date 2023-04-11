5621ad90ee668c060459e576c8fc420890a56ede
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-04-11 20:42:40 -0300

    se acomodo margen en las card y se sacaron linki de css en pagina principal

--- assets/js/ingreso.js
+++ assets/js/ingreso.js
@@ -2,10 +2,10 @@ const usuario = document.querySelector('[data-usuario]');
 const contraseña = document.querySelector('[data-contraseña]');
 const btn = document.querySelector('[data-btn]');
 function ingreso(){
-    if(usuario.value == 'usuario' && contraseña.value == 'usuario'){
-        window.location.href = './assets/pages/principal.html';
-    }else if(usuario.value == 'admin' && contraseña.value == 'admin'){
-        window.location.href = './assets/pages/administrador.html';
+    if(usuario.value === 'usuario' && contraseña.value === 'usuario'){
+        window.location.href = './assets/pages/usuario/principal.html';
+    }else if(usuario.value === 'admin' && contraseña.value == '=admin'){
+        window.location.href = './assets/pages/administrador/administrador.html';
     }else{
         alert('Usuario o contraseña incorrecto.')
 
